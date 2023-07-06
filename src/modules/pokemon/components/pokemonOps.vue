4bee9c6dfcfa740a11fe1d14505b0522d970d975
Daveq93 <cdavidq.22@gmail.com>
2023-07-05 19:46:51 -0500

    emit desde componentes Vue.js

--- src/modules/pokemon/components/pokemonOps.vue
+++ src/modules/pokemon/components/pokemonOps.vue
@@ -2,39 +2,54 @@
   <h1>pokemon options</h1>
   <div class="contenedor-opciones">
     <ul>
-    <li>1</li>
+      <!-- <li>1</li>
     <li>2</li>
     <li>3</li>
     <li>4</li>
-  </ul>
+    -->
+      <li
+        v-for="pokemon in opciones"
+        :key="pokemon.id"
+        @click="$emit('seleccionado',pokemon.id)"
+      >
+        {{ pokemon.nombre }}
+      </li>
+    </ul>
   </div>
-  
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    opciones: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    pruevaDeEvento() {},
+  },
+};
 </script>
 
 <style>
-ul{
+ul {
   list-style-type: none;
 }
-li{
-  border:1px solid rgba(0,0,0,0.2);
+li {
+  border: 1px solid rgba(0, 0, 0, 0.2);
   border-radius: 5px;
   background-color: aliceblue;
   margin-bottom: 10px;
   width: 200px;
   font-size: 25px;
   cursor: pointer;
-  
 }
-li:hover{
+li:hover {
   background-color: chocolate;
 }
-.contenedor-opciones{
-display:flex ;
-justify-content: center;
+.contenedor-opciones {
+  display: flex;
+  justify-content: center;
 }
-
 </style>
--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,26 +1,56 @@
 <template>
-  <h1>Juego Pokemon</h1>
-  <PokemonImg :pokemonId="251" :muestraPokemon="false"></PokemonImg>
-  <PokemonOps></PokemonOps>
+  <h1 v-if="pokemonCorrecto">Espere por favor!</h1>
+  <div v-el>
+    <h1>Juego Pokemon</h1>
+    <PokemonImg :pokemonId="pokemonCorrecto.id" :muestraPokemon="true" />
+    <PokemonOps
+      :opciones="pokemonArr"
+      @seleccionado="revisarSeleccion($event)"
+    />
+  </div>
 </template>
 
 <script>
 import PokemonImg from "../components/PokemonImg.vue";
 import PokemonOps from "../components/pokemonOps.vue";
+import obtenerFachadaPokemons from "../helpers/clientPokemonApi";
 
 export default {
-    components: {
+  components: {
     PokemonImg,
     PokemonOps,
+  },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemonCorrecto: null,
+      showPokemon: false,
+    };
+  },
+  methods: {
+    async cargaJuegoInicial() {
+      const arregloPokemons = await obtenerFachadaPokemons();
+      this.pokemonArr = this.arregloPokemons;
+      const indicePokemon = Math.floor(Math.random() * 4);
+      this.pokemonCorrecto = this.pokemonArr[indicePokemon];
+    },
+    revisarSeleccion(idSeleccionado) {
+      console.log("evento en el padre");
+      this.showPokemon = true;
+      console.log(idSeleccionado);
+      if (this.pokemonCorrecto == idSeleccionado) {
+      }
+    },
+  },
+  mounted() {
+    this.cargaJuegoInicial();
   },
 };
 </script>
 
-
-
 <style>
-h1,h2{
+h1,
+h2 {
   color: bisque;
 }
-
 </style>
